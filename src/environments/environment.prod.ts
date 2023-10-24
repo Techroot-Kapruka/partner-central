73c3d8a3f3089ed119fc41722645de12bff8894c
imash eshan <imashayesh@gmail.com>
2023-10-24 10:10:46 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   //test
   // baseURL : 'https://uat.partnercentral.lanka.info/kaprukaPartnerCentral/api/partnerCentral/',
   // baseURL : 'https://partnercentral.kapruka.com/kaprukaPartnerCentral/api/partnerCentral/',
+  //local base
   baseURL : '/api', // For the local run '/api'
   imageURIENV: '../../../../assets/images/product',
   imageURIENVTemp: '../../../../assets/images/editimagetemp'
