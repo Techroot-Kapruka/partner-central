faf122014250154bfeeaa1013825cb6c1d139ec8
dananjaya <asidananjaya123@gmail.com>
2023-12-07 14:46:44 +0530

    partner id set in product price edit

--- src/app/components/products/edit-products/edit-products.component.ts
+++ src/app/components/products/edit-products/edit-products.component.ts
@@ -1079,7 +1079,7 @@ export class EditProductsComponent implements OnInit {
       type: 'PRODUCT_PRICE',
       sub_type: 'product_price',
       comment: 'PRODUCT_PRICE',
-      requestedBy: this.vendorCode,
+      requestedBy: sessionStorage.getItem('partnerId'),
       userId: sessionStorage.getItem('userId'),
       data: [
         {
